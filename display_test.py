4de99591af80b731dc25ce8a6c171cd22cb62ace
huanyu <91276231+HuanyuL@users.noreply.github.com>
2024-06-03 15:59:36 +0000

    add zed camera for 3d recon

--- display_test.py
+++ display_test.py
@@ -18,14 +18,8 @@ class ImageDepthSubscriber:
         self.depth_sub = rospy.Subscriber(
             "/zedm/zed_node/depth/depth_registered", Image, self.depth_callback
         )
-
-        # Publish the raw depth image
-        self.depth_pub = rospy.Publisher(
-            "/zedm/zed_node/depth/depth_registered_raw", Image, queue_size=1
-        )
         self.rgb_image = None
         self.depth_image = None
-        self.depth_range = 2.0
 
     def rgb_callback(self, data):
         try:
@@ -37,23 +31,18 @@ class ImageDepthSubscriber:
         try:
             depth_image = self.bridge.imgmsg_to_cv2(data, "32FC1")
             self.depth_image = depth_image
-            # convert cv image to numpy array
-            scaled_depth = depth_image.astype(np.float32) / 255.0
-            depth_raw = scaled_depth * self.depth_range
-            depth_raw_msg = self.bridge.cv2_to_imgmsg(depth_raw, "32FC1")
-            self.depth_pub.publish(depth_raw_msg)
-
+            # depth_image = np.array(depth_image, dtype=np.int16)
+            print(np.max(depth_image))
+            print(np.min(depth_image))
         except CvBridgeError as e:
             rospy.logerr("CvBridge Error: {0}".format(e))
 
     def display_images(self):
         while not rospy.is_shutdown():
             if self.rgb_image is not None:
-                continue
-                # cv2.imshow("RGB Image", self.rgb_image)
+                cv2.imshow("RGB Image", self.rgb_image)
             if self.depth_image is not None:
-                continue
-                # cv2.imshow("Depth Image", self.depth_image)
+                cv2.imshow("Depth Image", self.depth_image)
 
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
